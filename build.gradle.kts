e7a91b868782700faf831c96716eb508285987c8
LinHeLurking <linhe.lurking@gmail.com>
2021-09-07 12:31:40 +0800

    resourece path renamed

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ configurations {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-log4j2:2.5.4")
     implementation("org.springframework.boot:spring-boot-starter-web:2.5.4")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.30")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.30")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.4")
