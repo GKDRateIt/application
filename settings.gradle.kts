b6c8814c28e828c586bcdebeae6750df8b21cc26
LinHeLurking <LinHe.Lurking@gmail.com>
2022-06-16 15:31:09 +0800

    Delete all old files for refactoring

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "RateItBackends"
-
-pluginManagement {
-    repositories {
-        maven {
-            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
-        }
-    }
-}
--- src/main/kotlin/com/github/rateit/heartbeat/HealthyChecker.kt
+++
@@ -1,8 +0,0 @@
-package com.github.rateit.heartbeat
-
-object HealthyChecker {
-    fun systemStatus():String{
-        // TODO: might change
-        return "Healthy"
-    }
-}
